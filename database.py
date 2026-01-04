f878a4e33f17e1e53e8449319ee82bb277484be3
Shubh Maurya <shubhmaurya040@gmail.com>
2026-01-04 13:29:00 +0530

    Use Railway MYSQL_URL instead of DATABASE_URL

--- database.py
+++ database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("mysql+pymysql://secure_user:secure123@localhost:3306/secure_notes")
+DATABASE_URL = os.getenv("MYSQL_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("MYSQL_URL not found")
 
 engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
