e0bd99cff070997f7671ca266572fc04b2cac06b
Shubh Maurya <shubhmaurya040@gmail.com>
2026-01-04 11:28:57 +0530

    Fix database url for production

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "mysql+pymysql://secure_user:secure123@localhost:3306/secure_notes"
+DATABASE_URL = os.getenv("mysql+pymysql://secure_user:secure123@localhost:3306/secure_notes")
 
 engine = create_engine(DATABASE_URL)
 
